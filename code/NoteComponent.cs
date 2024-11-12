427273982b47e19b100907a9a6692ebb580325ac
DrakeFruit <94070205+DrakeFruit@users.noreply.github.com>
2024-11-11 22:16:16 -0800

    Use switch expression for this rotation bullshit

--- code/NoteComponent.cs
+++ code/NoteComponent.cs
@@ -1,3 +1,4 @@
+using System;
 using Sandbox;
 
 public sealed class NoteComponent : Component
@@ -14,38 +15,24 @@ public sealed class NoteComponent : Component
 		if( noteData._type == 0 ) Model.Tint = LeftNoteColor;
 		else Model.Tint = RightNoteColor;
 		//Apply note rotation
-		float rot = 0;
-		switch( noteData._cutDirection )
-		{
-			case 0:
-				rot = 0;
-				break;
-			case 1:
-				rot = 180;
-				break;
-			case 2:
-				rot = -90;
-				break;
-			case 3:
-				rot = 90;
-				break;
-			case 4:
-				rot = -45;
-				break;
-			case 5:
-				rot = 45;
-				break;
-			case 6:
-				rot = -145;
-				break;
-			case 7:
-				rot = 145;
-				break;
-		}
+		int rot = ToRotation(noteData._cutDirection);
 		LocalRotation = new Angles( 0, 0, rot );
 	}
 	protected override void OnFixedUpdate()
 	{
 		LocalPosition += Vector3.Backward * NoteSpeed * Time.Delta;
 	}
+
+	private static int ToRotation(float cutDirection) => cutDirection switch
+	{
+		0 => 0,
+		1 => 180,
+		2 => -90,
+		3 => 90,
+		4 => -45,
+		5 => 45,
+		6 => -135, // was -145
+		7 => 135,  // was 145
+		_ => throw new ArgumentOutOfRangeException(nameof(cutDirection), cutDirection, null),
+	};
 }
